7149d83bbfae1429f3402385426e5122e799e88c
DaylonSrinivasan <daylon.srinivasan@gmail.com>
2025-01-01 23:56:24 +0000

    Use a background image that covers the whole page

--- README.md
+++ README.md
@@ -13,3 +13,5 @@ Open [http://localhost:3000](http://localhost:3000) with your browser to see the
 ## References
 
 Scroll animations: https://tutorials.emersonvisuals.com/tutorialseventeen
+
+Background image: https://www.freepik.com/free-photo/top-view-monochromatic-assortment-leaves-with-copy-space_11326670.htm
--- app/Components/Header/Header.tsx
+++ app/Components/Header/Header.tsx
@@ -1,25 +1,12 @@
 "use client";
 
 import React from "react";
-import Image from 'next/image';
 import { motion } from 'framer-motion';
-import BackgroundImage from '../../../public/background.jpg';
 
 
 const Header = () => {
     return (
         <main className="relative w-full h-screen">
-            {/* Background Image */}
-            <div className="absolute inset-0">
-                <Image
-                    src={BackgroundImage}
-                    alt="Background"
-                    layout="fill"
-                    objectFit="cover"
-                    className="z-0"
-                />
-            </div>
-
             {/* Animated Text */}
             <div className="absolute inset-0 flex items-center justify-center">
                 <motion.h1
