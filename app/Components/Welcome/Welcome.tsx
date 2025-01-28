2f6d4dada741b37728d9bbdc1e350e24f60d5967
DaylonSrinivasan <daylon.srinivasan@gmail.com>
2025-01-28 04:57:55 +0000

    Move engagement picture up

--- app/Components/Welcome/Welcome.tsx
+++ app/Components/Welcome/Welcome.tsx
@@ -18,17 +18,7 @@ const Welcome: React.FC<WelcomeProps> = ({ language }) => {
             <SectionHeader text={STRINGS.WELCOME[language]} />
 
             <div className="flex flex-col md:flex-row items-center justify-between mt-6"> 
-                <motion.div
-                    initial={{ opacity: 0 }}
-                    whileInView={{ opacity: 1 }}
-                    transition={{ duration: 1, ease: "easeOut", delay: 0.75 }}
-                    className="mt-6 md:mt-0 md:ml-6 text-left font-serif text-gray-800"
-                >
-                    {STRINGS.WELCOME_MESSAGE[language].map((paragraph, index) => (
-                        <p key={index} className="text-2xl leading-relaxed mb-4">{paragraph}</p>
-                    ))}
-                </motion.div>
-                <motion.div
+            <motion.div
                     initial={{ opacity: 0 }}
                     whileInView={{ opacity: 1 }}
                     transition={{ duration: 1, ease: "easeOut" }}
@@ -39,6 +29,16 @@ const Welcome: React.FC<WelcomeProps> = ({ language }) => {
                         alt="Engagement photo"
                         className="rounded-lg h-auto max-h-[50vh] w-full object-cover"
                     />
+                </motion.div>
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1 }}
+                    transition={{ duration: 1, ease: "easeOut", delay: 0.75 }}
+                    className="mt-6 md:mt-0 md:ml-6 text-left font-serif text-gray-800"
+                >
+                    {STRINGS.WELCOME_MESSAGE[language].map((paragraph, index) => (
+                        <p key={index} className="text-2xl leading-relaxed mb-4">{paragraph}</p>
+                    ))}
                 </motion.div>
             </div>
         </Section>
