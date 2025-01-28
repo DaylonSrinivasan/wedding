fdd70e763c91ca15e2d6b698816cd9ca5f0e7044
DaylonSrinivasan <daylon.srinivasan@gmail.com>
2025-01-28 04:52:19 +0000

    Add x and y padding

--- app/Components/Section/Section.tsx
+++ app/Components/Section/Section.tsx
@@ -7,12 +7,11 @@ type SectionProps = {
 
 const Section: React.FC<SectionProps> = ({ children, background }) => (
   <section
-    className="flex flex-col items-center justify-center min-h-screen w-screen bg-cover bg-center"
+    className="flex flex-col items-center justify-center min-h-screen w-screen bg-cover bg-center px-8 py-8"
     style={background ? { backgroundImage: `url(${background})` } : {}}
   >
     {children}
   </section>
 );
-
 
 export default Section;
