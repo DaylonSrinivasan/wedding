6629889435559eabebc53b6e5dbe4775421d7312
DaylonSrinivasan <daylon.srinivasan@gmail.com>
2025-01-28 04:55:18 +0000

    Reduce padding

--- app/Components/Section/Section.tsx
+++ app/Components/Section/Section.tsx
@@ -7,7 +7,7 @@ type SectionProps = {
 
 const Section: React.FC<SectionProps> = ({ children, background }) => (
   <section
-    className="flex flex-col items-center justify-center min-h-screen w-screen bg-cover bg-center px-8 py-8"
+    className="flex flex-col items-center justify-center min-h-screen w-screen bg-cover bg-center px-4 py-4"
     style={background ? { backgroundImage: `url(${background})` } : {}}
   >
     {children}
